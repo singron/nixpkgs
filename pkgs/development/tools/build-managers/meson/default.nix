f4c987d3a177af5d5bbc5fa1d14ab6d3195a3344
Eric Culp <eculperic@gmail.com>
2019-12-07 20:44:56 -0800

    meson: support mips cpu families

--- pkgs/development/tools/build-managers/meson/default.nix
+++ pkgs/development/tools/build-managers/meson/default.nix
@@ -10,11 +10,15 @@
 let
   # See https://mesonbuild.com/Reference-tables.html#cpu-families
   cpuFamilies = {
-    aarch64 = "aarch64";
-    armv6l  = "arm";
-    armv7l  = "arm";
-    i686    = "x86";
-    x86_64  = "x86_64";
+    aarch64  = "aarch64";
+    armv6l   = "arm";
+    armv7l   = "arm";
+    i686     = "x86";
+    mips     = "mips";
+    mipsel   = "mips";
+    mips64   = "mips64";
+    mips64el = "mips64";
+    x86_64   = "x86_64";
   };
 in
 python3Packages.buildPythonApplication rec {
